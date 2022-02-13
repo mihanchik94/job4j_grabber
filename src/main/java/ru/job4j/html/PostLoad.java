0e3248089cf4b46c70083d30b5558bdf8cc81529
mihanchik94 <mihanchik94@gmail.com>
2022-02-13 14:37:16 +0300

    2.3. Загрузка деталей поста. [#285212] (r.1.01)

--- src/main/java/ru/job4j/html/PostLoad.java
+++ src/main/java/ru/job4j/html/PostLoad.java
@@ -2,7 +2,6 @@ package ru.job4j.html;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 import ru.job4j.model.Post;
 
@@ -12,11 +11,10 @@ public class PostLoad {
     public static Post loadPost(String link) throws IOException {
         SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
         Document doc = Jsoup.connect(link).get();
-        Elements row = doc.select(".msgTable");
-        String[] titleArray = row.get(0).child(0).child(0).text().split("\\[");
+        String titleArray = doc.select(".messageHeader").get(0).ownText();
         String description = doc.select(".msgBody").get(1).text();
-        String[] dateTime = row.get(0).child(0).child(2).child(0).text().split("\\[");
-        return Post.getPost(0, titleArray[0], link, description, parser.parse(dateTime[0].substring(0, dateTime[0].length() - 1)));
+        String dateTime = doc.select(".msgFooter").get(0).text().substring(0, 16);
+        return Post.getPost(0, titleArray, link, description, parser.parse(dateTime));
     }
 
 
